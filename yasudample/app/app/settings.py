1166d81cd766bdc47ad2c16b321a48fcfee47c8d
rezorezo <d-higa@nttr.co.jp>
2017-03-21 16:17:16 +0900

    比嘉：朝夜メール(task/show.html)＆馬場：個人分析（indivisual.html）

--- yasudample/app/app/settings.py
+++ yasudample/app/app/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = secret_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["52.197.27.221","127.0.0.1"]
 
 
 # Application definition
